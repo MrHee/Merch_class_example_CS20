359d0beb292e0fef19e27071d036a7f424bc987c
Mr Hee <matteo.hee@spschools.org>
2023-02-03 08:46:58 -0700

    ArrayList and ForEACH Loop

---
+++ src/merch.java
@@ -0,0 +1,22 @@
+public class merch {
+
+    //Create our PROPERTIES
+
+   double price;
+   String name;
+   int quantityInStock;
+
+
+
+    //Constructor
+    merch(String name, double price, int quantityInStock){
+
+        this.name = name;
+        this.price = price;
+        this.quantityInStock=quantityInStock;
+
+    }
+
+
+
+}
